a3fdaae08aa7313d5244932d4d9ebb0d45faaf97
t_hirayama_gxpinc <t.hirayama@gxp.co.jp>
2023-11-19 21:50:42 +0900

    Add Supervisor Worker Project

---
+++ src/SchedulerAgentSupervisor.Scheduler.Supervisor/Program.cs
@@ -0,0 +1,10 @@
+using SchedulerAgentSupervisor.Scheduler.Supervisor;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
